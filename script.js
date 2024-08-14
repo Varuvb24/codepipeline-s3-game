69fc4d4a018696a4bdfd579c9a6618c879a6fa94
Varuvb24 <varuvj23@gmail.com>
2024-08-14 12:18:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardsWon = [];
 
     const cardArray = [
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
+        { name: 'card1', img: 'images/2.jpg' },
+        { name: 'card1', img: 'images/2.jpg' },
+        { name: 'card2', img: 'images/3.jpg' },
+        { name: 'card2', img: 'images/3.jpg' },
+        { name: 'card3', img: 'images/4.jpg' },
+        { name: 'card3', img: 'images/4.jpg' },
+        { name: 'card4', img: 'images/5.jpg' },
+        { name: 'card4', img: 'images/5.jpg' },
         { name: 'card5', img: 'images/success.png' },
         { name: 'card5', img: 'images/success.png' },
         // ...add more pairs as needed
@@ -75,3 +75,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
     startButton.addEventListener('click', createBoard);
 });
+
